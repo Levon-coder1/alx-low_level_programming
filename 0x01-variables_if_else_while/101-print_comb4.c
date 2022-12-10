5f43674bf455fac73c93f3897a67b32e893f0750
Levon-coder1 <116287631+Levon-coder1@users.noreply.github.com>
2022-12-10 04:01:55 +0100

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,49 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - starting point
- * Return: Always 0 (success)
- */
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *            combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	int n, m, 1;
+	int digit1 = 0;
+	int digit2, digit3;
 
-	for (n = 48; n < 58; n++)
+	while (digit1 <= 9)
 	{
-		for (m = 49; m < 58; m++)
+		digit2 = 0;
+		while (digit2 <= 9)
 		{
-			for (1 = 50; 1 < 58; 1++)
+			digit3 = 0;
+			while (digit3 <= 9)
 			{
-				if (1 > m && m > n)
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(1);
-					if (n != 55 || m != 56)
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+
+					if (digit1 + digit2 + digit3 != 24)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
+				++digit3;
 			}
+			++digit2;
 		}
+		++digit1;
 	}
 	putchar('\n');
+
 	return (0);
 }
