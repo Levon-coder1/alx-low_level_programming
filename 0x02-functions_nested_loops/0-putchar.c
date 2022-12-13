73e104d76e40d72ba72d132c8ef2efc0459680b3
Levon-coder1 <divineloveshizzy08@gmail.com>
2022-12-13 15:09:44 -0800

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - prints putchar and new line 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_puchar('\n');
+
+	return (0);
+}
