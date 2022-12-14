993782c9d73f8cd0e92116ba0446208b174af3d4
Levon-coder1 <divineloveshizzy08@gmail.com>
2022-12-13 18:35:17 -0800

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** 
+ * _abs - compute tue absoulte valie of am integer
+ * @n: the integer to be computed
+ *
+ * Return: the absolute valueof the integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
