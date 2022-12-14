9573a7b66ea5c3f577d0c0f0eddc7aa6cadc2a97
Levon-coder1 <116287631+Levon-coder1@users.noreply.github.com>
2022-12-14 04:41:33 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase
- * @c: character to check
- * Return: 1 if c is lowercase 0 therwise
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
-int _islower(int c)
+int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	return (c >= 'a' && c <= 'z');
 }
