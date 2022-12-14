c5783199290cf292c0e3e706f556c736da206a10
Levon-coder1 <divineloveshizzy08@gmail.com>
2022-12-13 16:36:06 -0800

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * take argument
+ * Return Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
